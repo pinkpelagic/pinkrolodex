886d8c69543a7432dabf55bf67bbb23bdc673187
NULL-BYTE <sonaligupta9802@gmail.com>
2020-08-09 16:20:36 +0530

    Updates

---
+++ precache-manifest.4cc2748e8b5a6ea7233bdd351b223fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a731f8d55563d229767c4778c4139d",
+    "url": "/pinkrolodex/index.html"
+  },
+  {
+    "revision": "ce74947f397f386953f1",
+    "url": "/pinkrolodex/static/css/main.5ea5de50.chunk.css"
+  },
+  {
+    "revision": "d724449ad7f93d363ba7",
+    "url": "/pinkrolodex/static/js/2.5d4b32d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pinkrolodex/static/js/2.5d4b32d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce74947f397f386953f1",
+    "url": "/pinkrolodex/static/js/main.90edee0a.chunk.js"
+  },
+  {
+    "revision": "4a57434b7192a0469402",
+    "url": "/pinkrolodex/static/js/runtime-main.f3f91275.js"
+  }
+]);
